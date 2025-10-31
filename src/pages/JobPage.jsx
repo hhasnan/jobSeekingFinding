445f88d91a51e174c2cf01d0f6b290d0e58d43b3
Muhammad Hasnan <109236264+hhasnan@users.noreply.github.com>
2025-10-31 10:20:59 +0500

    Add files via upload

---
+++ src/pages/JobPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import JobListing from '../components/JobListing'
+
+const JobPage = () => {
+    return (
+        <section>
+            <JobListing showAllCards={true}/>
+        </section>
+    )
+}
+
+export default JobPage
